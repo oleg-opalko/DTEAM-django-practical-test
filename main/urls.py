50bd1928f352c7a784ee9d3f204bbd56fbc2b04b
Oleh Opalko <oopalko96@gmail.com>
2025-06-01 23:24:45 +0300

    Finished task 2

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('cv/<int:cv_id>/', views.cv_detail, name='cv_detail'),
+    path('cv/<int:cv_id>/pdf/', views.render_pdf_view, name='cv_pdf'),
 ] 
--- main/views.py
+++ main/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+from django.template.loader import get_template
+from django.http import HttpResponse
 from .models import CV
+import io
+from xhtml2pdf import pisa
 
 # Create your views here.
 
@@ -11,3 +15,24 @@ def index(request):
 def cv_detail(request, cv_id):
     cv = get_object_or_404(CV.objects.filter(id=cv_id))
     return render(request, 'cv_detail.html', {'cv': cv})
+
+def render_to_pdf(template_src, context_dict):
+    template = get_template(template_src)
+    html = template.render(context_dict)
+    result = io.BytesIO()
+    pdf = pisa.pisaDocument(io.BytesIO(html.encode("UTF-8")), result)
+    if not pdf.err:
+        return result.getvalue()
+    return None
+
+def render_pdf_view(request, cv_id):
+    cv = get_object_or_404(CV, id=cv_id)
+    context = {
+        'cv': cv
+    }
+    pdf = render_to_pdf('cv_pdf.html', context)
+    if pdf:
+        response = HttpResponse(pdf, content_type='application/pdf')
+        response['Content-Disposition'] = f'attachment; filename="{cv.first_name}_{cv.last_name}_CV.pdf"'
+        return response
+    return HttpResponse("Error generating PDF", status=500)
